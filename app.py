ab1d4aa843005b3ddcc8609c6af51b65e731c16c
robertofd31 <75702301+robertofd31@users.noreply.github.com>
2024-06-27 13:41:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
 
+import streamlit as st
+import requests
+import pandas as pd
+
+# Definir la URL de la API
+url = 'https://yields.llama.fi/pools'
+
+# Hacer la solicitud GET a la API
+response = requests.get(url, headers={'accept': '*/*'})
+
+# Verificar si la solicitud fue exitosa
+if response.status_code == 200:
+    data = response.json()['data']
+
+    # Filtrar los resultados por el proyecto "notional"
+    filtered_data = [pool for pool in data if pool['project'] == 'notional']
+
+    # Convertir los datos filtrados en un DataFrame de pandas
+    df = pd.DataFrame(filtered_data)
+
+    # Mostrar los resultados en la aplicación Streamlit
+    st.title('Resultados de la API de Yields para el proyecto "notional"')
+    
+    if not df.empty:
+        st.dataframe(df)
+    else:
+        st.write("No se encontraron resultados para el proyecto 'notional'.")
+else:
+    st.write("Error al obtener los datos de la API.")
